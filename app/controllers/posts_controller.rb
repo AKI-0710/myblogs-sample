f9eb05f23bff1c5207b3870234fd96555fc21d40
Akito <kuroya1993@gmail.com>
2019-05-23 20:04:40 +0900

    implement post#create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,30 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.includes()
+  end
+
+  def create
+    post = Post.create(post_params)
+    binding.pry
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:article)
+  end
+
 end
